79165a28ea73e2b090f33fb3d82d686eec0fc1ba
Miras <200103493@stu.sdu.edu.kz>
2022-01-03 12:35:53 +0600

    updjs

--- js/script.js
+++ js/script.js
@@ -52,6 +52,24 @@ var swiper = new Swiper(".home-slider", {
     clickable: true,
   },
   loop:true,
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
 
 var swiper = new Swiper(".review-slider", {
